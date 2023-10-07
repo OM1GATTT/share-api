b6904020a8b6d58cd2246f1ce4abc8146cca9a38
OM1GA <845085139@qq.com>
2023-10-07 16:59:38 +0800

    演示 gateway 过滤器的使用

---
+++ share-gateway/src/main/java/top/om1ga/share/gateway/config/Test1Filter.java
@@ -0,0 +1,31 @@
+package top.om1ga.share.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author OM1GA
+ * @version 1.0
+ * @date 2023年10月07日 16:57
+ * @description Test1Filter
+ */
+@Component
+@Slf4j
+public class Test1Filter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Test1Filter");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}

a6ac306909295a3ec4a8a0c3e12d8c1d31acf38c
OM1GA <845085139@qq.com>
2023-10-07 15:35:05 +0800

    统一返回结果

--- share-user/src/main/java/top/om1ga/share/user/controller/UserController.java
+++ share-user/src/main/java/top/om1ga/share/user/controller/UserController.java
@@ -2,6 +2,7 @@ package top.om1ga.share.user.controller;
 
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
+import top.om1ga.share.common.resp.CommonResp;
 import top.om1ga.share.user.domain.dto.LoginDTO;
 import top.om1ga.share.user.domain.entity.User;
 import top.om1ga.share.user.service.UserService;
@@ -20,12 +21,18 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/count")
-    public Long count(){
-        return userService.count();
+    public CommonResp<Long> count(){
+        Long count = userService.count();
+        CommonResp<Long> commonResp = new CommonResp<>();
+        commonResp.setData(count);
+        return commonResp;
     }
 
     @PostMapping("/login")
-    public User login(@RequestBody LoginDTO loginDTO){
-        return userService.login(loginDTO);
+    public CommonResp<User> login(@RequestBody LoginDTO loginDTO){
+        User user = userService.login(loginDTO);
+        CommonResp<User> commonResp = new CommonResp<>();
+        commonResp.setData(user);
+        return commonResp;
     }
 }
